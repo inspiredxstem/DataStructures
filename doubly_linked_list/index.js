b2f4454f72b27c681158006b0430cbd2d58b2f8d
Steven Rosario <strosariogarcia@gmail.com>
2023-06-28 17:02:28 -0400

    Last functionalities for DLL

--- doubly_linked_list/index.js
+++ doubly_linked_list/index.js
@@ -92,4 +92,100 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    // if the index is less than 0 or greater than or equal to the length, return null
+    // if the index is less than or equal to half the length of the list, loop through the list
+    // starting from the head and loop towards the middle, returning the node once found
+    // if the index is greater than half the length of the list, loop through the list
+    // starting from the tail and loop towards the middle, return the node once found
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        // OPTIMIZED WAY OF GET FUNCTION
+        let count, current;
+        if(index <= this.length/2){
+            count = 0;
+            current = this.head;
+            while(count != index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(count != index){
+                current = current.prev;
+                count--;
+            }
+        }
+        return current; 
+
+        // UNOPTIMIZED WAY OF GET FUNCTION
+        // let count = 0;
+        // let current = this.head;
+        // while(count != index){
+        //     current = current.next;
+        //     count++;
+        // }
+        // return current;
+    }
+
+    // create a variable which is the result of the get method at the index passed
+    // if the get method returns a valid node, set the value of the node to be the value passed
+    // return true
+    // otherwise return false
+    set(index, val){
+        let foundNode = this.get(index);
+        if(foundNode){
+            foundNode.value = val;
+            return true;
+        }
+        return false;
+    }
+
+    // if the index is less than 0 or greater than or equal to the length of DLL, return false
+    // if index is 0, UNSHIFT
+    // if the index is the same as the length, PUSH
+    // use the get method to access the index - 1
+    // set the next and prev properties on the correct nodes to link everything together
+    // increment the length
+    // return true
+    insert(index, val){
+        if(index < 0 || index >= this.length) return false;
+        if(index === 0) return !!this.unshift(val);
+        if(index === this.length) return !!this.push(val);
+
+        let newNode = new Node(val);
+        let beforeNode = this.get(index - 1) ;
+        let afterNode = beforeNode.next;
+
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        afterNode.prev = newNode;
+
+        this.length++;
+        return true;
+    }
+
+    // if the index is less than 0 or greater than or equal to the length of DLL, return undefined
+    // if index is 0, SHIFT
+    // if the index is the same as the length-1, POP
+    // otherwise use the get method to retrieve the node to be removed
+    // update the next and prev property to remove the found node from the list
+    // set next and prev to null on the found node
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+
+        let removedNode = this.get(index);
+        removedNode.prev.next = removedNode.next;
+        removedNode.next.prev = removedNode.prev;
+        removedNode.next = null;
+        removedNode.prev = null;
+
+        this.length--;
+        return removedNode;;
+
+    }
 }
